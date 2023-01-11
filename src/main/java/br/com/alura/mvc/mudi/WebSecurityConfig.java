1cc8f9a0b0abf375030c7be1ee12e11885e812a5
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-01-11 17:37:00 -0300

    feat: add login.html and LoginController

---
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADM")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
